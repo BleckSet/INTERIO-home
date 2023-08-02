99b811e31e20dfd3c924bd9c2e42b03ce3821c10
BleckSet <108376728+BleckSet@users.noreply.github.com>
2023-08-02 16:06:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   $(".accordion-button").on("click", function() {
     var clickedAccordion = $(this);
@@ -9,4 +11,27 @@ $(document).ready(function() {
     var content = clickedAccordion.siblings(".accordion-content");
     content.slideToggle();
   });
+});
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const fadeElements = document.querySelectorAll('.appearance');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, {
+  threshold: 0.5 // Порог пересечения 0.5 означает, что элемент считается видимым, если видно 50% его области
+});
+
+fadeElements.forEach((fadeElement) => {
+  observer.observe(fadeElement);
 });
